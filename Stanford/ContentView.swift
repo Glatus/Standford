ce1dfab6cf01dc54a0577343e993905211663bc1
Glatus <89514030+Glatus@users.noreply.github.com>
2023-11-10 14:50:32 +0200

    Add files via upload

---
+++ Stanford/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Stanford
+//
+//  Created by iosdev on 8.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🦆","🦉","🦇","🦅","🦦","🦥","🦡","🦤"]
+    
+    @State var cardCount: Int = 4
+    
+    var body: some View {
+        VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+            ScrollView {
+                cards
+            }
+            Spacer()
+            themeChangers
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) {
+                index in CardView(content: emojis[index])
+                    .aspectRatio(2/3,contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var themeChangers: some View {
+        VStack {
+            Button(action: {
+                emojis = ["🍏","🍎","🍐","🍊","🍋","🍌","🍉","🍇",]
+                
+            }, label: { Image(systemName: "smiley")
+            })
+        }
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+        
+    }
+}
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    var body: some View {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ Stanford/StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordApp.swift
+//  Stanford
+//
+//  Created by iosdev on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
